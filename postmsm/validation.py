b5ca170172b398ff983ed804c55e3e64b8533af0
Saurabh Shukla <sshukla101@users.noreply.github.com>
2016-05-30 17:06:09 -0500

    Create validation.py

---
+++ postmsm/validation.py
@@ -0,0 +1,25 @@
+from msmbuilder.msm import MarkovStateModel
+from msmbuilder.cluster import KMeans
+import os
+import scipy.sparse, scipy.io
+import numpy as np
+
+################################################################################
+# Making clusters with different number of states along with MSM's with 
+# different lag times. Saves cluster object; MSM object and timescales pkl files
+################################################################################  
+def cluster_msm(sequences,n_states, lag_times):
+  for n in n_states:
+    states = KMeans(n_clusters=n)
+    states.fit(sequences)
+    io.dump(states,str(n)+'n_cl.pkl')
+    ts=np.zeros(5)
+    for lag_time in lag_times:
+        msm = MarkovStateModel(lag_time=lag_time, verbose=False,n_timescales=5)
+        msm.fit(states.labels_)
+        ts1=msm.timescales_
+        ts=np.vstack((ts,ts1))
+        io.dump(msm,str(n)+'n_'+str(lag_time)+'lt_msm.pkl')
+    ts=np.delete(ts, (0), axis=0)
+    io.dump(ts,str(n)+'n_timescales.pkl')
+  
