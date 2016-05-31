134d3dfcf4cd90442350a7276be21eb901a25bd5
Saurabh Shukla <sshukla101@users.noreply.github.com>
2016-05-31 15:44:38 -0500

    Create setup.py

---
+++ postmsm/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, Extension, find_packages
+
+
+__version__='0.1'
+
+setup(name='postmsm',
+      description='Post MSMBuilder analysis, for doing more biology, less coding',
+      author='Saurabh Shukla',
+      author_email='sshukla4@illinois.edu',
+      version=__version__,
+      
