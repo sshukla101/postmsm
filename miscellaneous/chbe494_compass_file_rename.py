624a1d5e0d919487fa251633517e9fa8e762477c
Saurabh Shukla <sshukla101@users.noreply.github.com>
2016-10-14 14:47:53 -0500

    Update chbe494_compass_file_rename.py

--- miscellaneous/chbe494_compass_file_rename.py
+++ miscellaneous/chbe494_compass_file_rename.py
@@ -7,3 +7,9 @@ for i in range(0,len(files)):
   temp=files[i].split('_')
   new_files.append(temp[4]+'_'+temp[5]+'_'+temp[6]+'_'+temp[7])
   os.rename(files[i],new_files[i])
+  
+  
+  txt_files=glob.glob(".txt")
+  
+  for f in txt_files:
+    os.remove(f)
